bed3c499143d9ffb2a3bbd9d546b8585f2179d16
user1 <you@example.com>
2021-04-27 21:37:40 +0530

    second commit

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
+
 variable "testrg" { type = string }
 variable "subnetA" { type = string }
 variable "TestVNet" { type = string }
 variable "terra_vault" { type = string }
 variable "Daily_backup" { type = string }
 #variable "Daily_backup" { type = string }
+# variable "test" {type = string}
