d1f08db47357ef5ff424a6a2fa03bc0747047d21
gunnasantosh <81152657+gunnasantosh@users.noreply.github.com>
2021-04-27 21:27:59 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "testrg" { type = string }
+variable "subnetA" { type = string }
+variable "TestVNet" { type = string }
+variable "terra_vault" { type = string }
+variable "Daily_backup" { type = string }
+#variable "Daily_backup" { type = string }
